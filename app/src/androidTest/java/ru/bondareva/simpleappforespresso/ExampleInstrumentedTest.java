d566cb11adef47984ddd21ee7d1cc880c063be37
Bondaal <160157215+Bondaal@users.noreply.github.com>
2025-09-29 13:19:01 +0300

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/ru/bondareva/simpleappforespresso/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/bondareva/simpleappforespresso/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package ru.kkuzmichev.simpleappforespresso;
+package ru.bondareva.simpleappforespresso;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ru.kkuzmichev.simpleappforespresso", appContext.getPackageName());
+        assertEquals("ru.bondareva.simpleappforespresso", appContext.getPackageName());
     }
 }
